4fb9fead634892e9db02f83dd346afb63e126928
zcaldwell <mintyst.john@gmail.com>
2021-10-07 13:23:52 -0700

    problem 2 done

--- src/math-functions.js
+++ src/math-functions.js
@@ -28,6 +28,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    const multiply = a * b;
+    const str = `The product of ${a} and ${b} is ${multiply}.`;
+    return [multiply, str];
 
 }
 
